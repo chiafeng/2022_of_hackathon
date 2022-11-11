db3e8537bf5c20f890341f920e130b8247626876
kingdom84521 <kingdomhwang@gmail.com>
2022-11-11 08:50:19 +0800

    mqtt subscribe via line

--- mqtt/publish.py
+++ mqtt/publish.py
@@ -3,12 +3,14 @@ import time
 
 
 def publish(number):
-    client = paho.Client()
-    # client.on_publish = on_publish
-    client.connect('broker.mqttdashboard.com', 1883)
-    client.loop_start()
+  client = paho.Client(client_id="test", clean_session=False)
+  # client.on_publish = on_publish
+  client.connect('broker.mqttdashboard.com', 1883)
+  client.loop_start()
 
-    while True:
-        msg = f"{number}"
-        client.publish('openfind123', msg)
-        time.sleep(1)
+  while True:
+    msg = f"{number}"
+    client.publish('openfind123', msg)
+    time.sleep(1)
+
+publish(84521)
--- mqtt/subscribe.py
+++ mqtt/subscribe.py
@@ -1,18 +1,28 @@
 import paho.mqtt.client as paho
-
+import reactivex as rx
+from reactivex import operators as ops
 
 def on_subscribe(client, userdata, mid, granted_qos):
-    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+  print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
+# def on_message(client, userdata, msg):
+#   # print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+#   data.next({
+#     "topic": msg.topic,
+#     "qos": msg.qos,
+#     "payload": msg.payload
+#   })
 
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+def on_disconnect(client, userdata, rc):
+  logging.info("disconnecting reason  "  +str(rc))
+  client.connected_flag=False
+  client.disconnect_flag=True
 
-
-def subscribe():
-    client = paho.Client()
-    client.on_subscribe = on_subscribe
-    client.on_message = on_message
-    client.connect('broker.mqttdashboard.com', 1883)
-    client.subscribe('openfind123')
-    client.loop_forever()
+def subscribe(on_message, on_subscribe=on_subscribe, on_disconnect=on_subscribe):
+  client = paho.Client()
+  client.on_subscribe = on_subscribe
+  client.on_message = on_message
+  client.on_disconnect = on_disconnect
+  client.connect('broker.mqttdashboard.com', 1883)
+  client.subscribe('openfind123')
+  client.loop_start()
